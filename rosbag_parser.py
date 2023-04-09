210a858dedb2ea246716f83da61f5770e347c65f
Paraskevas Chatzithanos <parischatz94@gmail.com>
2023-04-09 15:50:01 +0300

    Small refactoring on parse_rosbags

--- rosbag_parser.py
+++ rosbag_parser.py
@@ -5,13 +5,15 @@ import os
 import sys
 import pandas as pd
 
+# TODO Include logging module and remove prints
+
 class TopicNotFoundInBagError(Exception):
     pass
 
 class RosbagParser:
+    # TODO create docstrings
     def __init__(self, folder_path, topics_to_parse=[]):
         try:
-            #Raise FileNotFoundError if folder_path doesn't exist
             if not os.path.exists(folder_path):
                 raise FileNotFoundError
             self.folder_path = folder_path
@@ -23,10 +25,8 @@ class RosbagParser:
 
     def check_topics(self, topic_list):
         try:
-            # Get all topics from each rosbag
             for file in os.listdir(self.folder_path):
                 if file.endswith(".bag"):
-                    # Open rosbag file
                     with rosbag.Bag(os.path.join(self.folder_path, file)) as bag:
                         topics = list(bag.get_type_and_topic_info().topics.keys())
                         if topic_list[0]!="all":
@@ -43,32 +43,27 @@ class RosbagParser:
             print(f"{repr(e)}: Topics {different_topics} not found in {file}")
             sys.exit(1)
 
-    def parse_rosbags(self,topics=None, save_csv=True):
-            if topics is None:
-                topics = self.topics_to_parse
-            for file in os.listdir(self.folder_path):
-                if file.endswith(".bag"):
-                    df = pd.DataFrame(columns=topics)
-                    with rosbag.Bag(os.path.join(self.folder_path, file)) as bag:
-                        rows = []
-                        for topic, msg, t in bag.read_messages(topics=topics):
-                            row = {}
-                            for col_topic in topics:
-                                row[col_topic] = None
-                            row[topic] = msg
-                            row['Timestamp'] = t
-                            rows.append(row)
-                        df = pd.concat([df, pd.DataFrame(rows)], ignore_index=True)
-                        bag.close()
+    def parse_rosbags(self, topics=None, save_csv=True):
+        topics = topics or self.topics_to_parse
+        for file in os.listdir(self.folder_path):
+            if file.endswith(".bag"):
+                df = pd.DataFrame(columns=topics)
+                with rosbag.Bag(os.path.join(self.folder_path, file)) as bag:
+                    rows = []
+                    for topic, msg, t in bag.read_messages(topics=topics):
+                        row = {col_topic: None for col_topic in topics}
+                        row[topic] = msg
+                        row['Timestamp'] = t
+                        rows.append(row)
+                    df = pd.concat([df, pd.DataFrame(rows)], ignore_index=True)
+                if save_csv:
+                    filename = file.split(".")[0]
+                    df.set_index('Timestamp', inplace=True)
+                    df.to_csv(os.path.join(self.folder_path, f'{filename}.csv'), index=True)
 
-                        if save_csv:
-                            filename = file.split(".")[0]
-                            df.set_index('Timestamp', inplace=True)
-                            df.to_csv(os.path.join(self.folder_path,f'{filename}.csv'), index=True)
 
 if __name__ == '__main__':
 
-    # Get rosbag folder path from command line argument
     if len(sys.argv) <= 1:
         print("Please provide the path to the rosbag folder when running the script. \nExample: python3 rosbag_parser.py /path/to/rosbag_folder")
         sys.exit(1)
@@ -76,7 +71,6 @@ if __name__ == '__main__':
 
     parser = RosbagParser(folder_path)
 
-    # Get topics to parse from user input
     topics = input("Enter the topics you want to parse (separate multiple topics with space or type all): ")
     topics = topics.split()
 
