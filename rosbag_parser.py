23360b8ddf91eeefb70ff32fe3e51d57ba9e7fce
Paraskevas Chatzithanos <parischatz94@gmail.com>
2023-04-11 19:38:12 +0300

    Debuging and refactoring

--- rosbag_parser.py
+++ rosbag_parser.py
@@ -4,8 +4,10 @@ import rosbag
 import os
 import sys
 import pandas as pd
+import logging
 
-# TODO Include logging module and remove prints
+logging.basicConfig(level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+
 
 class TopicNotFoundInBagError(Exception):
     pass
@@ -13,6 +15,7 @@ class TopicNotFoundInBagError(Exception):
 class RosbagParser:
     # TODO create docstrings
     def __init__(self, folder_path, topics_to_parse=[]):
+        self.setup_logging()
         try:
             if not os.path.exists(folder_path):
                 raise FileNotFoundError
@@ -20,8 +23,13 @@ class RosbagParser:
             self.topics_to_parse = topics_to_parse
         
         except FileNotFoundError as e:
-            print(f"{repr(e)}: The specified folder does not exist. Please check if path given is correct.")
+            logging.error(f"{repr(e)}: The specified folder does not exist. Please check if path given is correct.")
             sys.exit(1)
+
+    def setup_logging(self):
+        logging.basicConfig(format='%(asctime)s %(levelname)-8s %(message)s',
+                            datefmt='%Y-%m-%d %H:%M:%S',
+                            level=logging.DEBUG)
 
     def get_topics(self, topic_list):
         try:
@@ -40,7 +48,7 @@ class RosbagParser:
                             self.topics_to_parse = topics
 
         except TopicNotFoundInBagError as e:
-            print(f"{repr(e)}: Topics {different_topics} not found in {file}")
+            logging.error(f"{repr(e)}: Topics {different_topics} not found in {file}")
             sys.exit(1)
     
     def parse_topics(self):
@@ -54,7 +62,7 @@ class RosbagParser:
         recurrent_topics = all_topics[0]
         for topic_list in all_topics:
             recurrent_topics = list(set(recurrent_topics).intersection(topic_list))
-        print(recurrent_topics)
+        self.topics_to_parse = recurrent_topics
 
     def parse_rosbags(self, topics=None, save_csv=True):
         topics = topics or self.topics_to_parse
@@ -62,6 +70,8 @@ class RosbagParser:
             if file.endswith(".bag"):
                 df = pd.DataFrame(columns=topics)
                 with rosbag.Bag(os.path.join(self.folder_path, file)) as bag:
+                    # TODO Initializing the rows list with a fixed size instead 
+                    # of appending to it every iteration of the loop.
                     rows = []
                     for topic, msg, t in bag.read_messages(topics=topics):
                         row = {col_topic: None for col_topic in topics}
@@ -77,26 +87,27 @@ class RosbagParser:
 def main():
     try:
         if len(sys.argv) <= 1:
-            print("Please provide the path to the rosbag folder when running the script. \nExample: python3 rosbag_parser.py /path/to/rosbag_folder")
+            logging.error("Please provide the path to the rosbag folder when running the script. \nExample: python3 rosbag_parser.py /path/to/rosbag_folder")
             sys.exit(1)
         folder_path = sys.argv[1]
         parser = RosbagParser(folder_path)
         while True:
             topics = input("Enter the topics you want to parse (separate multiple topics with space or type all): ")
-            topics = topics.split()
-            if any(topic.startswith("/") for topic in topics) or topics[0]=="all":
+            topics = topics.lower().split()
+            if all(topic.startswith("/") for topic in topics) or topics[0]=="all":
                 break
-            print("Error: All topics must start with '/'")
+            logging.error("Error: All topics must start with '/'")
         # Check the topics
         parser.get_topics(topics)
         # Parse rosbags
         parser.parse_rosbags()
 
     except KeyboardInterrupt:
-        print("\nProgram interrupted by user")
+        print("\n")
+        logging.error("Program interrupted by user")
 
 if __name__ == '__main__':
     main()
-    # Testing comments
+    # # Testing comments
     # parser = RosbagParser("/home/paris/Projects/rosbag_parser/rosbags")
     # parser.parse_topics()
